92f2bbe61d67a1c53a0419f925d243358f298362
Lenovo <Lenovo@LAPTOP-8OMEVQ48>
2019-01-16 07:12:23 +0530

    Simple java program

---
+++ MyApp/src/SingleValueContainer.java
@@ -0,0 +1,17 @@
+
+public class SingleValueContainer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         byte age=10;
+         age=18;
+         System.out.println("age is:"+age);
+         char ch='A';
+         ch=66;
+         System.out.println("value is:"+ch);
+         boolean isInternetConnected=true;
+         isInternetConnected=false;
+         System.out.println("result is:"+isInternetConnected);
+	}
+
+}
